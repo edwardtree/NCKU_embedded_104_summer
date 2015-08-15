2c9c3330ce50952d8f927038e7b6c65179a6a1ca
edwardtree <edwardsmalltree@yahoo.com.tw>
2015-08-15 19:17:31 +0800

    Add Create_PhoneBook function.
    We deleted original code which can create PhoneBook in main function. We defaulted the size of PhoneBook is 100.

--- 104_08_10/homework2.c
+++ 104_08_10/homework2.c
@@ -3,6 +3,7 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
+
 typedef struct PhoneBook{
     char LastName[MAX_LAST_NAME_SIZE];
     char FirstName[16];
@@ -16,6 +17,7 @@ typedef struct PhoneBook{
     char zip[5];
     struct PhoneBook *pNext;
 } PhoneBook;
+
 
 PhoneBook FindName(char Last[], PhoneBook *pHead) {
     
@@ -40,39 +42,59 @@ PhoneBook FindName(char Last[], PhoneBook *pHead) {
     return *ptr3;
 }
 
-int main(void)
-{
-	
-	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) * 10);
-	
-	int i,j;
-	
-	struct PhoneBook *ptr1 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
-	struct PhoneBook *ptr2 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
-	
-	sprintf( ptr1 -> LastName, "aa") ; 
+PhoneBook Create_PhoneBook(void){
 
+	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) * 100);
+	
+	int i,j,k=0;
 	
 	//先建立一個一維指標陣列	
 	for( i=0 ; i<10 ; i++)
 	{
-		sprintf( (ptr+i) -> LastName, "N%c" ,'a'+ i) ;
-		(ptr+i) -> pNext = NULL;
+		for( j=0 ; j<10 ; j++ )
+		{
+			sprintf( (ptr+k ) -> LastName, "N%c%c" ,'a'+ i, 'a'+j) ;
+			(ptr+k) -> pNext = NULL;
+			k++;
+		}
 	}
 	
 	//把陣列裡面的每個entry串連起來
-	for( i=0 ; i<10 ; i++ )
+	for( i=0 ; i<100 ; i++ )
 	{
-		if( i != 9 )
+		if( i != 99 )
 			(ptr+i) -> pNext = ptr+i+1;
-	}	
+	}
+
+return *ptr;
+}
+
+
+
+
+int main(void)
+{
 	
-	*ptr2 = FindName( ptr1 -> LastName ,ptr );			
+	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) * 100);	
+	struct PhoneBook *ptr1 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
+	struct PhoneBook *ptr2 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
 	
+	int i,j;
+
+	*ptr = Create_PhoneBook();//Default 
+
+	//for( j=0 ; j<16 ; j++)
+	//	printf("ptr3 = %c\n ", ptr -> LastName[j]);
+		
+	sprintf( ptr1 -> LastName, "Ne");
+	//*ptr2 = FindName( ptr1 -> LastName ,ptr );			
 	
+	/*	
 	for( j=0 ; j<16 ; j++)
 		printf("%c ", ptr2 -> LastName[j]);
-	/*		
+	*/
+	
+			
 	//印出	
 	while( ptr != NULL )
 	{ 
@@ -81,7 +103,7 @@ int main(void)
 		printf("\n");
 		ptr = ptr -> pNext;
 	}
-	*/
+	
 	return 0 ;
 	
 }
