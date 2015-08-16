15d548cf7e6110879f2a013cd0d157e2eecc46b8
edwardtree <edwardsmalltree@yahoo.com.tw>
2015-08-17 02:08:35 +0800

    Add Direct mapped Cache and CPU address.
    When CPU want to store data in memory. We can observe CPU address to know some information.
    
    |  tag  |  index  |  byte offset |
    
    tag : Tag can detect the  data whather or not is turely in cache.
    index : Index can show us that CPU want to store which cache entry.
    byre offset : Which byte block need to store in the data of cache.
    
    ---------------------------
    
    Cache information :
    
    index : 0 -> don't have data in this entry; 1 -> have data in this entry.
    tag : Tag can detect the  data whather or not is turely in cache.
    data : PhoneBook LastName.
    
    ----------------------------
    
    When memory want to store the data in the cache, the mapping function :
    (memory block address ) mod ( number of cache blocks in the cache)
    
    ---------------------------
    
    the test_address.txt can work, but address.txt can not work.

--- 104_08_10/homework2.c
+++ 104_08_10/homework2.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define MAX_LAST_NAME_SIZE 16
-
+#define PHONEBOOK_SIZE 100
+#define CACHE_TAG_SIZE 7
+#define CACHE_SIZE 4
+#define ADDRESS_NUMBER 6
 
 typedef struct PhoneBook{
     char LastName[MAX_LAST_NAME_SIZE];
@@ -19,36 +23,23 @@ typedef struct PhoneBook{
 } PhoneBook;
 
 
-PhoneBook FindName(char Last[], PhoneBook *pHead) {
-    
-    int i,j,k;
-    
-    while (pHead != NULL) {
-        if (strcmp(Last, pHead->LastName) == 0) 
-	{
-    	
-		return *pHead;
-        }
-	pHead = pHead->pNext;
-    }
-	
-   struct PhoneBook *ptr3 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
-   
-   sprintf( ptr3 -> LastName, "Nothing find") ; 
-     
-    return *ptr3;
-}
+typedef struct Direct_Mapped_Cache{
+	char index[1];
+	int tag;
+	char data[MAX_LAST_NAME_SIZE];
+	struct Direct_Mapped_Cache *Next;
+}Direct_Mapped_Cache;
 
 PhoneBook Create_PhoneBook(void){
 
-	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) * 100);
+	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) * PHONEBOOK_SIZE);
 	
 	int i,j,k=0;
 	
-	//先建立一個一維指標陣列	
-	for( i=0 ; i<10 ; i++)
+	//Constuct a pointer array	
+	for( i=0 ; i<sqrt(PHONEBOOK_SIZE) ; i++)
 	{
-		for( j=0 ; j<10 ; j++ )
+		for( j=0 ; j<sqrt(PHONEBOOK_SIZE)  ; j++ )
 		{
 			sprintf( (ptr+k ) -> LastName, "N%c%c" ,'a'+ i, 'a'+j) ;
 			(ptr+k) -> pNext = NULL;
@@ -56,37 +47,101 @@ PhoneBook Create_PhoneBook(void){
 		}
 	}
 	
-	//把陣列裡面的每個entry串連起來
-	for( i=0 ; i<100 ; i++ )
+	//Connect each array entry
+	for( i=0 ; i<PHONEBOOK_SIZE ; i++ )
 	{
-		if( i != 99 )
+		if( i != PHONEBOOK_SIZE-1 )
 			(ptr+i) -> pNext = ptr+i+1;
 	}
 
-return *ptr;
+	return *ptr;
 }
 
+Direct_Mapped_Cache Create_Cache(void){
 
+	struct Direct_Mapped_Cache *ptr = (struct Direct_Mapped_Cache*)malloc(sizeof(struct Direct_Mapped_Cache) * CACHE_SIZE);
+	
+	int i;
+	
+	for( i=0 ; i<CACHE_SIZE ; i++)
+	{
+		sprintf( (ptr+i ) -> index, "%c" ,'0') ;
+		(ptr+i) -> Next = NULL;
+	}
+	
+	for( i=0 ; i< CACHE_SIZE ; i++ )
+	{
+		if( i != CACHE_SIZE-1 )
+			(ptr+i) -> Next = ptr+i+1;
+	}
+	return *ptr;	
+}
 
 
 int main(void)
 {
 	
-	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) * 100);	
-	struct PhoneBook *ptr1 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
-	struct PhoneBook *ptr2 = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ));
-	
+	struct PhoneBook *ptr = (struct PhoneBook*)malloc(sizeof(struct PhoneBook ) *PHONEBOOK_SIZE );	
+	struct Direct_Mapped_Cache *ptr3 = (struct Direct_Mapped_Cache*)malloc(sizeof(struct Direct_Mapped_Cache)*CACHE_SIZE);
+	struct Direct_Mapped_Cache *ptr3_front = (struct Direct_Mapped_Cache*)malloc(sizeof(struct Direct_Mapped_Cache)*CACHE_SIZE);
+
+	FILE *fptr_output;
+	fptr_output = fopen("Address.txt","r");	
+
 	int i,j;
 
-	*ptr = Create_PhoneBook();//Default 
+	*ptr = Create_PhoneBook();//Default phonebook size is 100. 
+	*ptr3 = Create_Cache();// Default Cache size is 4.
 	
-	sprintf( ptr1 -> LastName, "Nea");
-	
-	*ptr2 = FindName( ptr1 -> LastName ,ptr );			
-	
+	ptr3_front -> Next = ptr3;// Record the address of first cache
+
+	int Address_tag, Address_index, Address_byte_offset;// CPU byte address : tage, index, byte offset.
 		
-	for( j=0 ; j<16 ; j++)
-		printf("%c\n", ptr2 -> LastName[j]);
+	//Testing the number of CPU address 
+	for( i = 0 ; i < ADDRESS_NUMBER ; i++ )
+	{
+		fscanf( fptr_output, "%d %d %d " , &Address_tag, &Address_index, &Address_byte_offset);
+		
+		/* 106 ~ 110 : record which cache need to detect.*/
+		while( Address_index > 0 )
+		{
+			ptr3 = ptr3 -> Next;
+			Address_index--;
+		}
+		
+		/* 113 ~ 118 : Record which phonebook need to replace when cache miss. */
+		int point_to_which_phonebook_entry = Address_tag;
+		while( point_to_which_phonebook_entry > 0 )
+		{
+			ptr = ptr -> pNext;	
+			point_to_which_phonebook_entry--;	
+		}
+
+		if( ptr3->index[0] == '0' )
+		{
+
+			printf("Compulsory Miiss\n");
+			sprintf( ptr3 -> index, "%c" ,'1');			
+			ptr3 -> tag = Address_tag;
+			for( j=0 ; j<16 ; j++)
+                             ptr3 -> data[j] = ptr -> LastName[j]; 
+		}
+		else if( ptr3->index[0] == '1' &&  ptr3 -> tag != Address_tag)
+		{
+			printf("Conflict Miss\n");
+			ptr3 -> tag = Address_tag;
+			for( j=0 ; j<16 ; j++)
+                             ptr3 -> data[j] = ptr -> LastName[j]; 
+		}
+		else
+		{
+			printf("Cache Hit\n");
+		}
+	
+		ptr3 = ptr3_front -> Next;		
+	}
+	
+	fclose(fptr_output);		
 	
 	return 0 ;
 	
